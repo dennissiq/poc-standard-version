21183ec18adb40969acea3df56a50f997d09ce9c
Dennis Siqueira <ti.dennissiqueira@gmail.com>
2023-04-18 10:13:30 -0300

    fix: main text

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> edit main file
+          EDITADO <code>src/App.tsx</code> edit main file
         </p>
       </div>
       <p className="read-the-docs">
