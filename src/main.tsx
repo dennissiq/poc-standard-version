612cdcaf64019ce5b1fece4919b6250012d0b74e
Dennis Siqueira <ti.dennissiqueira@gmail.com>
2023-04-18 14:51:58 -0300

    feat'!': main text

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 // add comments
 // teste
+// outro teste
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
